f2408233c0df37a306a4046332bac27a44d84628
JustinVerkade <justin.n.verkade@gmail.com>
2023-12-14 10:51:42 +0100

    Day 14

--- Core/Src/AoC/Days/Day13.c
+++ Core/Src/AoC/Days/Day13.c
@@ -7,11 +7,274 @@
 
 #include "AoC/Days/Day13.h"
 
+#define PROCESS_STATE_FETCHING 0
+#define PROCESS_STATE_PROCESSING 1
+#define PROCESS_STATE_RESETTING 2
+
+int32_t getVertical(char *map, uint32_t width, uint32_t depth, uint32_t exeption)
+{
+	// check for vertical mirror
+	uint32_t vertical = 0;
+	for(uint32_t w=1; w<width; w++)
+	{
+		if(w == exeption)
+			continue;
+
+		// get maximum width
+		uint32_t max_mirror_width = ((width - w) < (w)) ? width - w: w;
+
+		// check rows for simularity
+		uint8_t mirrored = 1;
+		for(uint8_t i=0; i<max_mirror_width; i++)
+		{
+			for(uint8_t j=0; j<depth; j++)
+			{
+				// get compare indices
+				uint32_t index0 = (j * (width + 1)) + (w - i - 1);
+				uint32_t index1 = (j * (width + 1)) + (w + i + 0);
+				if(map[index0] != map[index1])
+				{
+					mirrored = 0;
+					break;
+				}
+			}
+			if(!mirrored)
+				break;
+		}
+		if(mirrored)
+		{
+			vertical = w;
+			break;
+		}
+	}
+	return vertical;
+}
+
+int32_t getHorizontal(char *map, uint32_t width, uint32_t depth, uint32_t exeption)
+{
+	// check for horizontal mirror
+	int32_t horizontal = 0;
+	for(uint32_t d=1; d<depth; d++)
+	{
+		if(d == exeption)
+			continue;
+
+		// get maximum width
+		uint32_t max_mirror_depth = ((depth - d) < (d)) ? depth - d: d;
+
+		// check rows for simularity
+		uint8_t mirrored = 1;
+		for(uint8_t i=0; i<max_mirror_depth; i++)
+		{
+			for(uint8_t j=0; j<width; j++)
+			{
+				// get compare indices
+				uint32_t index0 = ((d - i - 1) * (width + 1)) + (j);
+				uint32_t index1 = ((d + i + 0) * (width + 1)) + (j);
+				if(map[index0] != map[index1])
+				{
+					mirrored = 0;
+					break;
+				}
+			}
+			if(!mirrored)
+				break;
+		}
+		if(mirrored)
+		{
+			horizontal = d;
+			break;
+		}
+	}
+	return horizontal;
+}
+
 AdventOfCode_return_t AoC_Day_13(AdventOfCode_config_t config)
 {
 	// implementation state (change to OK when the day is active)
 	if(config == AOC_CONFIG_TEST)
-		return AOC_RETURN_NOK;
+		return AOC_RETURN_OK;
+
+	{ // part 1
+		// fetch file
+		FIL file;
+		uint8_t ret;
+		if(config == AOC_CONFIG_DEBUG_VERBOSE)
+			ret = f_open(&file, "day13t.txt", FA_READ);
+		else ret = f_open(&file, "day13.txt", FA_READ);
+		if(ret != FR_OK)
+		{
+			AdventOfCode_print(config, "    Cannot find file!\n", strlen("\tCannot find file!\n"));
+			return AOC_RETURN_NOK;
+		}
+
+		// create character buffer
+		char *map = (char*)sram_calloc(512, 1);
+
+		// process data
+		uint32_t sum = 0;
+		uint8_t state = PROCESS_STATE_FETCHING;
+		char buffer[64];
+		while(f_gets(buffer, 64, &file))
+		{
+			// fetching
+			if(state == PROCESS_STATE_FETCHING)
+			{
+				if(buffer[0] == '\n')
+					state = PROCESS_STATE_PROCESSING;
+				else strcat(map, buffer);
+				if(map[strlen(map) - 1] != '\n')
+				{
+					strcat(map, "\n");
+					state = PROCESS_STATE_PROCESSING;
+				}
+			}
+
+			// processing
+			if(state == PROCESS_STATE_PROCESSING)
+			{
+				// get map dimension
+				uint32_t width = 0;
+				for(; map[width]!='\n'; width++);
+				uint32_t depth = strlen(map) / (width + 1);
+
+				int32_t vertical = getVertical(map, width, depth, 0);
+				int32_t horizontal = getHorizontal(map, width, depth, 0);
+
+				// total
+				uint32_t total = vertical + 100 * horizontal;
+				sum += total;
+
+				state = PROCESS_STATE_RESETTING;
+			}
+
+			// resetting
+			if(state == PROCESS_STATE_RESETTING)
+			{
+				memset(map, 0x00, 256);
+				state = PROCESS_STATE_FETCHING;
+			}
+		}
+
+		// output answer
+		char answer[64];
+		sprintf(answer, "    Part 1 answer: %lu\n", sum);
+		AdventOfCode_print(config, answer, strlen(answer));
+
+		// clean up
+		f_close(&file);
+		sram_clrmem();
+	}
+
+	{ // part 1
+		// fetch file
+		FIL file;
+		uint8_t ret;
+		if(config == AOC_CONFIG_DEBUG_VERBOSE)
+			ret = f_open(&file, "day13t.txt", FA_READ);
+		else ret = f_open(&file, "day13.txt", FA_READ);
+		if(ret != FR_OK)
+		{
+			AdventOfCode_print(config, "    Cannot find file!\n", strlen("\tCannot find file!\n"));
+			return AOC_RETURN_NOK;
+		}
+
+		// create character buffer
+		char *map = (char*)sram_calloc(512, 1);
+
+		// process data
+		uint32_t sum = 0;
+		uint8_t state = PROCESS_STATE_FETCHING;
+		char buffer[64];
+		while(f_gets(buffer, 64, &file))
+		{
+			// fetching
+			if(state == PROCESS_STATE_FETCHING)
+			{
+				if(buffer[0] == '\n')
+					state = PROCESS_STATE_PROCESSING;
+				else strcat(map, buffer);
+				if(map[strlen(map) - 1] != '\n')
+				{
+					strcat(map, "\n");
+					state = PROCESS_STATE_PROCESSING;
+				}
+			}
+
+			// processing
+			if(state == PROCESS_STATE_PROCESSING)
+			{
+				// get map dimension
+				uint32_t width = 0;
+				for(; map[width]!='\n'; width++);
+				uint32_t depth = strlen(map) / (width + 1);
+
+				// get current result
+				int32_t vertical = getVertical(map, width, depth, 0);
+				int32_t horizontal = getHorizontal(map, width, depth, 0);
+				int32_t new_vertical = 0;
+				int32_t new_horizontal = 0;
+
+				// go over each line and row index
+				char clone[512];
+				uint8_t flag = 0;
+				for(uint8_t w=0; w<width; w++)
+				{
+					for(uint8_t d=0; d<depth; d++)
+					{
+						// make a clone of the target
+						memcpy(clone, map, strlen(map) + 1);
+
+						// swap smudge
+						uint32_t index = d * (width + 1) + w;
+						clone[index] = clone[index] == '.' ? '#' : '.';
+
+						// get current result
+						new_vertical = getVertical(clone, width, depth, vertical);
+						new_horizontal = getHorizontal(clone, width, depth, horizontal);
+
+						// abort if failed
+						if(!new_vertical && !new_horizontal)
+							continue;
+
+						flag = 1;
+						break;
+					}
+					if(flag)
+						break;
+				}
+
+				if(!flag)
+				{
+					AdventOfCode_print(config, "Failed!\n", strlen("Failed!\n"));
+				}
+
+				// total
+				if(new_vertical != vertical)
+					sum += new_vertical;
+				if(new_horizontal != horizontal)
+					sum += 100 * new_horizontal;
+
+				state = PROCESS_STATE_RESETTING;
+			}
+
+			// resetting
+			if(state == PROCESS_STATE_RESETTING)
+			{
+				memset(map, 0x00, 256);
+				state = PROCESS_STATE_FETCHING;
+			}
+		}
+
+		// output answer
+		char answer[64];
+		sprintf(answer, "    Part 2 answer: %lu\n", sum);
+		AdventOfCode_print(config, answer, strlen(answer));
+
+		// clean up
+		f_close(&file);
+		sram_clrmem();
+	}
 
 	// status code
 	return AOC_RETURN_OK;
--- Core/Src/AoC/Days/Day14.c
+++ Core/Src/AoC/Days/Day14.c
@@ -7,12 +7,305 @@
 
 #include "AoC/Days/Day14.h"
 
+void printRocks(char *rocks, uint32_t width, uint32_t depth)
+{
+	for(uint32_t line=0; line<depth; line++)
+	{
+		uint32_t index = line * (width + 1);
+		char tx_buffer[256];
+		sprintf(tx_buffer, "%s\n", &rocks[index]);
+		AdventOfCode_print(AOC_CONFIG_DEBUG_VERBOSE, tx_buffer, strlen(tx_buffer));
+		HAL_Delay(10);
+	}
+}
+
+void rollNorth(char *rocks, uint32_t width, uint32_t depth)
+{
+	// find rocks from top to bottom
+	for(uint32_t d=1; d<depth; d++)
+	{
+		for(uint32_t w=0; w<width; w++)
+		{
+			// get current index
+			uint32_t index = d * (width+1) + w;
+
+			// abort if index is not a rolling rock
+			if(rocks[index] != 'O')
+				continue;
+
+			// clear current index
+			rocks[index] = '.';
+
+			// find the destination position where the rock will land
+			int32_t d2;
+			for(d2=d; d2>0; d2--)
+				if(rocks[(d2 - 1) * (width+1) + w] != '.')
+					break;
+
+			// reposition rock
+			rocks[d2 * (width+1) + w] = 'O';
+		}
+	}
+}
+
+void rollWest(char *rocks, uint32_t width, uint32_t depth)
+{
+	// find rocks from top to bottom
+	for(uint32_t w=1; w<width; w++)
+	{
+		for(uint32_t d=0; d<depth; d++)
+		{
+			// get current index
+			uint32_t index = d * (width+1) + w;
+
+			// abort if index is not a rolling rock
+			if(rocks[index] != 'O')
+				continue;
+
+			// clear current index
+			rocks[index] = '.';
+
+			// find the destination position where the rock will land
+			int32_t w2;
+			for(w2=w; w2>0; w2--)
+				if(rocks[d * (width+1) + (w2 - 1)] != '.')
+					break;
+
+			// reposition rock
+			rocks[d * (width+1) + w2] = 'O';
+		}
+	}
+}
+
+void rollSouth(char *rocks, uint32_t width, uint32_t depth)
+{
+	// find rocks from top to bottom
+	for(int32_t d=depth-2; d>=0; d--)
+	{
+		for(int32_t w=0; w<width; w++)
+		{
+			// get current index
+			uint32_t index = d * (width+1) + w;
+
+			// abort if index is not a rolling rock
+			if(rocks[index] != 'O')
+				continue;
+
+			// clear current index
+			rocks[index] = '.';
+
+			// find the destination position where the rock will land
+			int32_t d2;
+			for(d2=d; d2<depth-1; d2++)
+				if(rocks[(d2 + 1) * (width+1) + w] != '.')
+					break;
+
+			// reposition rock
+			rocks[d2 * (width+1) + w] = 'O';
+		}
+	}
+}
+
+void rollEast(char *rocks, uint32_t width, uint32_t depth)
+{
+	// find rocks from top to bottom
+	for(int32_t w=width-2; w>=0; w--)
+	{
+		for(uint32_t d=0; d<depth; d++)
+		{
+			// get current index
+			uint32_t index = d * (width+1) + w;
+
+			// abort if index is not a rolling rock
+			if(rocks[index] != 'O')
+				continue;
+
+			// clear current index
+			rocks[index] = '.';
+
+			// find the destination position where the rock will land
+			int32_t w2;
+			for(w2=w; w2<width-1; w2++)
+				if(rocks[d * (width+1) + (w2 + 1)] != '.')
+					break;
+
+			// reposition rock
+			rocks[d * (width+1) + w2] = 'O';
+		}
+	}
+}
+
+uint64_t calculateLoad(char *rocks, uint32_t width, uint32_t depth)
+{
+	// calculate each rocks weight
+	uint64_t load = 0;
+	for(uint32_t d=0; d<depth; d++)
+		for(uint32_t w=0; w<width; w++)
+			if(rocks[d * (width+1) + w] == 'O')
+				load += depth - d;
+	return load;
+}
+
 AdventOfCode_return_t AoC_Day_14(AdventOfCode_config_t config)
 {
 	// implementation state (change to OK when the day is active)
 	if(config == AOC_CONFIG_TEST)
-		return AOC_RETURN_NOK;
+		return AOC_RETURN_OK;
+
+	{ // part 1
+		// fetch file
+		FIL file;
+		uint8_t ret;
+		if(config == AOC_CONFIG_DEBUG_VERBOSE)
+			ret = f_open(&file, "day14t.txt", FA_READ);
+		else ret = f_open(&file, "day14.txt", FA_READ);
+		if(ret != FR_OK)
+		{
+			AdventOfCode_print(config, "    Cannot find file!\n", strlen("\tCannot find file!\n"));
+			return AOC_RETURN_NOK;
+		}
+
+		// get file length
+		int32_t file_size = f_size(&file);
+
+		// extract data
+		char *rocks = (char*)sram_malloc(file_size + 1);
+
+		// insert data
+		f_read(&file, rocks, file_size + 1, NULL);
+
+		// close file
+		f_close(&file);
+
+		// get space dimensions and insert line ends
+		char *token = strtok(rocks, "\n");
+		uint32_t width = strlen(token);
+		uint32_t depth = 1;
+		while(strtok(NULL, "\n"))
+			depth++;
+
+		// roll rocks
+		rollNorth(rocks, width, depth);
+
+		// calculate load
+		uint64_t load = calculateLoad(rocks, width, depth);
+
+		// output answer
+		char answer[64];
+		sprintf(answer, "    Part 1 answer: %llu\n", load);
+		AdventOfCode_print(config, answer, strlen(answer));
+
+		// clean up
+		sram_clrmem();
+	}
+
+	{ // part 2
+		// fetch file
+		FIL file;
+		uint8_t ret;
+		if(config == AOC_CONFIG_DEBUG_VERBOSE)
+			ret = f_open(&file, "day14t.txt", FA_READ);
+		else ret = f_open(&file, "day14.txt", FA_READ);
+		if(ret != FR_OK)
+		{
+			AdventOfCode_print(config, "    Cannot find file!\n", strlen("\tCannot find file!\n"));
+			return AOC_RETURN_NOK;
+		}
+
+		// get file length
+		int32_t file_size = f_size(&file);
+
+		// extract data
+		char *rocks = (char*)sram_malloc(file_size + 1);
+
+		// insert data
+		f_read(&file, rocks, file_size + 1, NULL);
+
+		// close file
+		f_close(&file);
+
+		// get space dimensions and insert line ends
+		char *token = strtok(rocks, "\n");
+		uint32_t width = strlen(token);
+		uint32_t depth = 1;
+		while(strtok(NULL, "\n"))
+			depth++;
+
+		// cache memory
+		uint32_t *memory = (uint32_t*)sram_malloc(sizeof(uint32_t) * 10000);
+		uint32_t memory_ptr = 0;
+
+		uint32_t sequence_start = 0;
+		uint32_t sequence_length = 0;
+		uint32_t cycles = 10000;
+		for(uint32_t cycle=0; cycle<cycles; cycle++)
+		{
+			// roll rocks
+			rollNorth(rocks, width, depth);
+			rollWest(rocks, width, depth);
+			rollSouth(rocks, width, depth);
+			rollEast(rocks, width, depth);
+
+			// calculate load
+			uint32_t load = calculateLoad(rocks, width, depth);
+			memory[memory_ptr] = load;
+			memory_ptr++;
+
+			// check if sequence
+			if(cycle < 8)
+				continue;
+
+			uint8_t exit = 0;
+			for(uint32_t seq_len=2; seq_len<memory_ptr/2; seq_len++)
+			{
+				uint8_t flag = 0;
+				uint32_t index0 = memory_ptr - 1 - seq_len * 0;
+				uint32_t index1 = memory_ptr - 1 - seq_len * 1;
+				for(uint32_t length=0; length<seq_len; length++)
+				{
+					if(memory[index0 - length] != memory[index1 - length])
+					{
+						flag = 1;
+						break;
+					}
+				}
+
+				if(!flag)
+				{
+					sequence_start = index1 - seq_len + 1;
+					sequence_length = seq_len;
+					exit = 1;
+					break;
+				}
+			}
+			if(exit)
+				break;
+		}
+
+		// calculcate pattern leftovers
+		uint32_t leftover_rolls = (1000000000 - sequence_start) % sequence_length;
+		for(uint32_t roll=0; roll<leftover_rolls; roll++)
+		{
+			rollNorth(rocks, width, depth);
+			rollWest(rocks, width, depth);
+			rollSouth(rocks, width, depth);
+			rollEast(rocks, width, depth);
+		}
+
+		// calculate load
+		uint64_t load = calculateLoad(rocks, width, depth);
+
+		// output answer
+		char answer[64];
+		sprintf(answer, "    Part 2 answer: %llu\n", load);
+		AdventOfCode_print(config, answer, strlen(answer));
+
+		// clean up
+		sram_clrmem();
+
+	}
 
 	// status code
 	return AOC_RETURN_OK;
 }
+
